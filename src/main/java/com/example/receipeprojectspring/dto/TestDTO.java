31ea341d4850c7879939f55597e082c4080d958b
KimuYounguWoo <dkp0803@gmail.com>
2022-12-12 20:24:30 +0900

    Commit - Connect Database

--- src/main/java/com/example/receipeprojectspring/dto/TestDTO.java
+++ src/main/java/com/example/receipeprojectspring/dto/TestDTO.java
@@ -6,7 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class TestDTO {
-    private String name;
-    private int age;
-    private String email;
+    private String EMPNAME;
+    private int EMPNO;
+    private String TITLE;
+    private int DNO;
+    private String HIREDATE;
+    private double SALARY;
+    private int MANAGER;
 }
